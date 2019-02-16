81af51d8fc4fc9ae445eb99d4c414e3f2e05b200
Eric Jefferson <ejefferson50@gmail.com>
2019-02-16 17:12:29 -0500

    Fininshed working on last test

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -6,30 +6,129 @@ package com.zipcodewilmington.person;
 public class Person {
     private String name;
     private int age;
+    private int height;
+    private int weight;
+    private String hairColor;
+    private String gender;
+    private Boolean hasSiblings;
 
     public Person() {
+        this.name = "";
+        this.age = Integer.MAX_VALUE;
+        this.height = Integer.MAX_VALUE;
+        this.weight = Integer.MAX_VALUE;
+        this.hairColor = "";
+        this.gender = "";
+        this.hasSiblings = false;
+
     }
 
     public Person(int age) {
+        this.age = age;
+
+
     }
 
     public Person(String name) {
+        this.name = name;
+
+
     }
 
+
+
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+
+
+    }
+
+    public Person(int height, int weight) {
+        this.height = height;
+        this.weight = weight;
+
+
+    }
+
+    public Person(String gender) {
+        this.gender = gender;
+
+    }
+
+    public Person(Boolean hasSiblings) {
+        this.hasSiblings = hasSiblings;
+
+
     }
 
     public void setName(String name) {
+    this.name = name;
+
     }
 
     public void setAge(int age) {
+    this.age = age;
+    }
+
+    public void setHeight(Integer height){
+        this.height = height;
+    }
+
+    public void setWeight(Integer weight){
+        this.weight = weight;
+    }
+
+    public void setHairColor(String hairColor){
+        this.hairColor = hairColor;
+    }
+
+    public void setGender(String gender){
+        this.gender = gender;
+    }
+
+    public void setHasSiblings(boolean hasSiblings){
+        this.hasSiblings = hasSiblings;
     }
 
     public String getName() {
-        return null;
+
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+
+        return age;
     }
+
+    public Integer getHeight() {
+
+        return height;
+
+    }
+
+    public Integer getWeight() {
+
+        return weight;
+
+    }
+
+    public String getHairColor() {
+
+        return hairColor;
+
+    }
+
+    public String getGender() {
+
+        return gender;
+
+    }
+
+    public Boolean getHasSiblings() {
+
+        return hasSiblings;
+
+    }
+
 }
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -12,6 +12,11 @@ public class TestPerson {
         // Given
         String expectedName = "";
         Integer expectedAge = Integer.MAX_VALUE;
+        Integer expectedHeight = Integer.MAX_VALUE;
+        Integer expectedWeight = Integer.MAX_VALUE;
+        String expectedHairColor = "";
+        String expectedGender = "";
+        Boolean expectedHasSiblings = true;
 
         // When
         Person person = new Person();
@@ -19,6 +24,11 @@ public class TestPerson {
         // Then
         String actualName = person.getName();
         Integer actualAge = person.getAge();
+        Integer actualHeight = person.getHeight();
+        Integer actualWeight = person.getWeight();
+        String actualHairColor = person.getHairColor();
+        String actualGender = person.getGender();
+        Boolean actualHasSiblings = person.getHasSiblings();
 
         Assert.assertEquals(expectedName, actualName);
         Assert.assertEquals(expectedAge, actualAge);
@@ -30,7 +40,7 @@ public class TestPerson {
         String expected = "Leon";
 
         // When
-        Person person = new Person(expected);
+        Person person = new Person();
 
         // Then
         String actual = person.getName();
@@ -69,6 +79,23 @@ public class TestPerson {
     }
 
     @Test
+    public void testConstructorWithHeightAndWeight(){
+        //Given
+        Integer expectedHeight = 6;
+        Integer expectedWeight = 225;
+
+        //When
+        Person person = new Person(expectedHeight, expectedWeight);
+
+        //Then
+        Integer actualHeight = person.getHeight();
+        Integer actualWeight = person.getWeight();
+
+        Assert.assertEquals(expectedHeight, actualHeight);
+        Assert.assertEquals(expectedWeight, actualWeight);
+    }
+
+    @Test
     public void testSetName() {
         // Given
         Person person = new Person();
@@ -95,4 +122,76 @@ public class TestPerson {
         Integer actual = person.getAge();
         Assert.assertEquals(expected, actual);
     }
+
+    @Test
+    public void testSetHeight() {
+        //Given
+        Person person = new Person();
+        Integer expected = 6;
+
+        //When
+        person.setHeight(expected);
+
+        //Then
+        Integer actual = person.getHeight();
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testSetWeight() {
+        //Given
+        Person person = new Person();
+        Integer expected = 225;
+
+        //When
+        person.setWeight(expected);
+
+        //Then
+        Integer actual = person.getWeight();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getHairColor() {
+        //Given
+        Person person = new Person();
+        String expected = "brown";
+
+        //When
+        person.setHairColor(expected);
+
+        //Then
+        String actual = person.getHairColor();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getGender() {
+        //Given
+        Person person = new Person();
+        String expected = "Male";
+
+        //When
+        person.setGender(expected);
+
+        //Then
+        String actual = person.getGender();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getHasSiblings() {
+        //Given
+        Person person = new Person();
+        Boolean expected = true;
+
+        //When
+        person.setHasSiblings(expected);
+
+        //Then
+        Boolean actual = person.getHasSiblings();
+        Assert.assertEquals(expected, actual);
+    }
+
 }
